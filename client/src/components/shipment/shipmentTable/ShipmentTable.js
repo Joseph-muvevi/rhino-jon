905a33f88e446e196968a3a09f5956a4ebd9f032
SamsAgency <samstore637@gmail.com>
2021-09-07 12:31:39 +0300

    working with the table

--- client/src/components/shipment/shipmentTable/ShipmentTable.js
+++ client/src/components/shipment/shipmentTable/ShipmentTable.js
@@ -6,51 +6,53 @@ import axios from "axios";
 import TheTable from "./table";
 
 const ShipmentTable = () => {
-  const [searchParam, setSearchParam] = useState("storageshipment")
-  const [shipment, setShipments] = useState([]);
-  const [trackno, setTrackno] = useState([])
+  const [tracktype, setTracktype] = useState("storageshipment");
+  const [trackno, setTrackno] = useState("")
+  const [shipment, setShipment] = useState([])
 
+  const handleSelect = (e) => {
+    setTracktype(e.target.value)
+  }
   
-  const formik = useFormik({
-    initialValues : {
-        tracktype: "",
-        tracknumber: ""
-    },
-    validationSchema: Yup.object().shape({
-        tracktype: Yup.string()
-            .required()
-            .min(3)
-            .max(50),
-        tracknumber: Yup.string()
-            .required()
-            .min(3)
-            .max(50)
-    }), 
-    onSubmit: (values, {resetForm}) => {
-        alert(JSON.stringify(values, null, 2))
+  const handleTrackno = (e) => {
+    setTrackno(e.target.value)
+  }
+
+//   const formik = useFormik({
+//     initialValues : {
+//         tracktype: "",
+//         tracknumber: ""
+//     },
+//     validationSchema: Yup.object().shape({
+//         tracktype: Yup.string()
+//             .required()
+//             .min(3)
+//             .max(50),
+//         tracknumber: Yup.string()
+//             .required()
+//             .min(3)
+//             .max(50)
+//     }), 
+//     onSubmit: (values, {resetForm}) => {
+//         alert(JSON.stringify(values, null, 2))
         
-        resetForm({
-            initialValues : ""
-        })
-    }
-})
+//         resetForm({
+//             initialValues : ""
+//         })
+//     }
+// })
+
+// storageshipment
 
   useEffect(() => {
-    const getShipments = () => {
-      axios
-      .get("http://localhost:8080/api/storageshipment")
-      .then((res) => console.log(res.data))
-      .then((object) => setShipments(object.data))
-      .catch((err) => console.log(err));
+    try {
+      axios.get(`http://localhost:8080/api/${tracktype}`)
+      .then(res => setShipment(res.data))
+      .then(console.log(shipment))
+    } catch (err) {
+      console.log(err)
     }
-    const getStorage = () => {
-      axios
-      .get("http://localhost:8080/api/storageshipment")
-      .then((res) => console.log(res.data))
-      .then((object) => setShipments(object.data))
-      .catch((err) => console.log(err));
-    }
-  }, []);
+  }, [tracktype, trackno]);
 
   return (
     <div className="shipping">
@@ -60,32 +62,30 @@ const ShipmentTable = () => {
                         flexDirection: "column", 
                         justifyContent: "center",
                         alignItems: "center",
-                        marginTop: "20px" }} onSubmit={formik.handleSubmit}>
+                        marginTop: "20px" }} >
             <select type="text"  name="tracktype" 
-                    onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.tracktype} required>
+                    onChange={handleSelect}  value={tracktype} required>
                         <option value="" disabled label="Please select tracking options"/>
                         <option value="logisticsrecords" label="Track Shipment" />
                         <option value="storageshipment" label="Truck Storage"/>
                     </select>
-                    {formik.touched.tracktype && formik.errors.tracktype ? (
-                        <div className="error">{formik.errors.tracktype}</div>
-                    ) : null}<br/>
+                    <br/>
             <input type="text" placeholder="Insert Track number" name="tracknumber" 
-                    onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.tracknumber} required/>
-                    {formik.touched.tracknumber && formik.errors.tracknumber ? (
-                        <div className="error">{formik.errors.tracknumber}</div>
-                    ) : null}
+                    onChange={handleTrackno} value={trackno} required/>
         </form>
-        {shipment ? (
-          <TheTable key={shipment.id} data={shipment} />
-        ) : (
-          <div className="shipment-table-content">
-            <h1 className="shipment-table-header">Nothin here yet</h1>
-            <p className="shipment-table-p"></p>
-          </div>
-        )}
-        {/* {console.log(shipment, "This is from the shipment data")} */}
       </div>
+      {shipment ? (
+        <TheTable key={shipment.id} data={shipment} />
+      ) : (
+        <div className="shipment-table-content">
+          <h1 className="shipment-table-header">Nothin here yet</h1>
+          <p className="shipment-table-p"></p>
+        </div>
+      )}
+      {/* {console.log(shipment, "This is from the shipment data")} */}
+      {shipment.map(items => {
+        return console.log(items)
+      })}
     </div>
   );
 };
